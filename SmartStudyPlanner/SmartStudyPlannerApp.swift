0ad9e7d66bbc28f21f432cd805a6b5d2ec5d495b
Pubudu Perera <pubupere32@gmail.com>
2026-03-23 20:43:10 +0530

    Initial Commit

---
+++ SmartStudyPlanner/SmartStudyPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartStudyPlannerApp.swift
+//  SmartStudyPlanner
+//
+//  Created by Pubudu Perera on 2026-03-23.
+//
+
+import SwiftUI
+
+@main
+struct SmartStudyPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
